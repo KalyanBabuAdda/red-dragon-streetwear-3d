8f8e53b58ac2b78443951583e2b421ef147c956e
Kalyan Babu Adda <kalyanbabuadda40@gmail.com>
2026-09-05 17:03:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ if (heroShirt && heroProduct) {
      * Rotation speed.
      * Higher number = faster rotation.
      */
-    const rotationSpeed = 0.025;
+    const rotationSpeed = 0.03;
 
 
     /* ========================================
